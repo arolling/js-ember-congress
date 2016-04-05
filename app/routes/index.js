91090c279cf989daadab4c1f1a57afb37fd8c912
Abigail Rolling and Jessica Fix <jcfix.pdx@gmail.com>
2016-04-05 09:21:33 -0700

    Add API to index route to display committees; generate committee route for individual committees

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    var url = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&per_page=50&apikey=882877952f00411b8e6d7b7a89045c5e';
+    return Ember.$.getJSON(url).then(function(responseJSON){
+      return responseJSON.results;
+    });
+  }
 });
