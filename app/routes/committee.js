e026a8bc1f04cc9363534ebf3c82ddc4f0e660f5
Abigail Rolling and Jessica Fix <arolling@gmail.com>
2016-04-05 11:51:07 -0700

    Display all bills for a committee

--- app/routes/committee.js
+++ app/routes/committee.js
@@ -2,9 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    var url = 'http://congress.api.sunlightfoundation.com/committees?fields=subcommittees,name,url&per_page=all&committee_id=' + params.committee_id + '&apikey=882877952f00411b8e6d7b7a89045c5e';
-    return Ember.$.getJSON(url).then(function(responseJSON){
-      return responseJSON.results;
+    var urlCommittee = 'http://congress.api.sunlightfoundation.com/committees?fields=subcommittees,name,committee_id,url,members&per_page=all&committee_id=' + params.committee_id + '&apikey=882877952f00411b8e6d7b7a89045c5e';
+    var urlBills = 'http://congress.api.sunlightfoundation.com/bills?committee_ids=' + params.committee_id + '&apikey=882877952f00411b8e6d7b7a89045c5e';
+    return Ember.RSVP.hash({
+      committee: Ember.$.getJSON(urlCommittee).then(function(responseJSON){
+        return responseJSON.results;
+      }),
+      bills: Ember.$.getJSON(urlBills).then(function(responseJSON){
+        console.log(responseJSON.results);
+        return responseJSON.results;
+      })
+
     });
-  }
+  },
+
+  // afterModel: function(params){
+  //   console.log(params[0].committee_id);
+  //   var url = 'http://congress.api.sunlightfoundation.com/bills?committee_ids=' + params[0].committee_id + '&apikey=882877952f00411b8e6d7b7a89045c5e';
+  //   return Ember.$.getJSON(url).then(function(responseJSON){
+  //     console.log(responseJSON.results);
+  //     return responseJSON.results;
+  //   });
+  // }
+
+
 });
